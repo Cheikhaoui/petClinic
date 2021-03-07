b2d1885fdcf48512ac2092dd45980e8b19a1ad4e
hcheikhaoui <hicham.cheikhaoui@docaposte.fr>
2021-03-07 10:37:09 +0100

    Implement Pet Clinic POJO Data Model

---
+++ src/main/java/hc/springframework/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package hc.springframework.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/hc/springframework/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package hc.springframework.petclinic.model;
+
+public class Vet extends Person {
+}
