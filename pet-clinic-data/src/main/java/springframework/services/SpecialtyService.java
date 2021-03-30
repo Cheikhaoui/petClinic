2a1d787008f50b48f9833aeef62b337e5b60a180
hcheikhaoui <hicham.cheikhaoui@docaposte.fr>
2021-03-31 00:31:57 +0100

    Create specialty map service Impl close #29

---
+++ pet-clinic-data/src/main/java/springframework/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package springframework.services;
+
+import springframework.model.Speciality;
+
+public interface SpecialtyService extends CrudService<Speciality,Long> {
+}
---
+++ pet-clinic-data/src/main/java/springframework/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package springframework.services.map;
+
+import org.springframework.stereotype.Service;
+import springframework.model.Speciality;
+import springframework.services.SpecialtyService;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Speciality,Long> implements SpecialtyService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
